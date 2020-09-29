c7cb7ae31e398326d2eeb81876a0764ca60810ae
Nicholas Stankevich <nicholas.stankevich@gmail.com>
2020-09-29 13:35:55 +0200

    fixed bug when clicking to submit a new comment. New bug that broke the posts link

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import api from '../utils/api';
 import { setAlert } from './alert';
 import {
   DELETE_POST,
@@ -14,7 +14,7 @@ import {
 // Get posts
 export const getPosts = () => async (dispatch) => {
   try {
-    const res = await axios.get('/api/posts');
+    const res = await api.get('/posts');
 
     dispatch({
       type: GET_POSTS,
@@ -31,7 +31,7 @@ export const getPosts = () => async (dispatch) => {
 // Add like
 export const addLike = (id) => async (dispatch) => {
   try {
-    const res = await axios.put(`/api/posts/like/${id}`);
+    const res = await api.put(`/posts/like/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -48,7 +48,7 @@ export const addLike = (id) => async (dispatch) => {
 // Remove like
 export const removeLike = (id) => async (dispatch) => {
   try {
-    const res = await axios.put(`/api/posts/unlike/${id}`);
+    const res = await api.put(`/posts/unlike/${id}`);
 
     dispatch({
       type: UPDATE_LIKES,
@@ -65,7 +65,7 @@ export const removeLike = (id) => async (dispatch) => {
 // Delete post
 export const deletePost = (id) => async (dispatch) => {
   try {
-    await axios.delete(`/api/posts/${id}`);
+    await api.delete(`/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -89,7 +89,7 @@ export const addPost = (formData, id) => async (dispatch) => {
     },
   };
   try {
-    await axios.post(`/api/posts`, formData, config);
+    await api.post(`/posts`, formData, config);
 
     dispatch({
       type: ADD_POST,
@@ -108,7 +108,7 @@ export const addPost = (formData, id) => async (dispatch) => {
 // Get post
 export const getPost = (id) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/posts/${id}`);
+    const res = await api.get(`/posts/${id}`);
 
     dispatch({
       type: GET_POST,
@@ -130,11 +130,7 @@ export const addComment = (postId, formData) => async (dispatch) => {
     },
   };
   try {
-    const res = await axios.post(
-      `/api/posts/comment/${postId}`,
-      formData,
-      config
-    );
+    const res = await api.post(`/posts/comment/${postId}`, formData, config);
 
     dispatch({
       type: ADD_COMMENT,
@@ -153,7 +149,7 @@ export const addComment = (postId, formData) => async (dispatch) => {
 // Delete comment
 export const deleteComment = (postId, commentId) => async (dispatch) => {
   try {
-    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    await api.delete(`/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,

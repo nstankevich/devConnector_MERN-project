c744a5fd724f38e33e08cc21797dcd5e83c3e8fe
Nicholas Stankevich <nicholas.stankevich@gmail.com>
2020-09-29 14:47:35 +0200

    debugged components profile folder files.

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -12,10 +12,10 @@ const ProfileTop = ({
   },
 }) => {
   return (
-    <div class='profile-top bg-primary p-2'>
-      <img class='round-img my-1' src={avatar} alt='' />
-      <h1 class='large'>{name}</h1>
-      <p class='lead'>
+    <div className='profile-top bg-primary p-2'>
+      <img className='round-img my-1' src={avatar} alt='' />
+      <h1 className='large'>{name}</h1>
+      <p className='lead'>
         {status} {company && <span> at {company}</span>}
       </p>
       <p>{location && <span>{location}</span>}</p>

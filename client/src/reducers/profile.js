3cffc4f16f56afc28acd17e730a6262532fc0ff6
Nicholas Stankevich <nicholas.stankevich@gmail.com>
2020-09-30 17:52:25 +0200

    deleted files

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -5,7 +5,7 @@ import {
   UPDATE_PROFILE,
   GET_PROFILES,
   GET_REPOS,
-  NO_REPOS
+  NO_REPOS,
 } from '../actions/types';
 
 const initialState = {
@@ -13,7 +13,7 @@ const initialState = {
   profiles: [],
   repos: [],
   loading: true,
-  error: {}
+  error: {},
 };
 
 export default function (state = initialState, action) {
@@ -25,37 +25,37 @@ export default function (state = initialState, action) {
       return {
         ...state,
         profile: payload,
-        loading: false
+        loading: false,
       };
     case GET_PROFILES:
       return {
         ...state,
         profiles: payload,
-        loading: false
+        loading: false,
       };
     case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
         loading: false,
-        profile: null
+        profile: null,
       };
     case CLEAR_PROFILE:
       return {
         ...state,
         profile: null,
-        repos: []
+        repos: [],
       };
     case GET_REPOS:
       return {
         ...state,
         repos: payload,
-        loading: false
+        loading: false,
       };
     case NO_REPOS:
       return {
         ...state,
-        repos: []
+        repos: [],
       };
     default:
       return state;
